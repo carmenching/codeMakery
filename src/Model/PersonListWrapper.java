e54c2d966481bdc9ab86de1ca8acbac9d2576c4b
Carmen BURNY <carmen.burny@jimenezfva.com>
2019-08-23 17:13:16 +0200

    Add maven module and jaxb dependency

---
+++ src/Model/PersonListWrapper.java
@@ -0,0 +1,29 @@
+package Model;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "persons")
+public class PersonListWrapper {
+    /**
+     * Helper class to wrap a list of persons. This is used for saving the
+     * list of persons to XML.
+     *
+     * @author Marco Jakob
+     */
+
+        private List<Person> persons;
+
+        @XmlElement(name = "person")
+        public List<Person> getPersons() {
+            return persons;
+        }
+
+        public void setPersons(List<Person> persons) {
+            this.persons = persons;
+        }
+
+
+}
